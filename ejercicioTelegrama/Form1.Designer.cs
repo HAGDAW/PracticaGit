beb8043e752875ef5507936b938aa3bf72449cf8
HÉCTOR ANDREU GARCÍA <hecandgar@alu.edu.gva.es>
2025-11-09 18:41:21 +0100

    Añadir codigo inicial con errores

--- ejercicioTelegrama/Form1.Designer.cs
+++ ejercicioTelegrama/Form1.Designer.cs
@@ -28,21 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.RichTextBox();
+            this.chkUrgente = new System.Windows.Forms.CheckBox();
+            this.lblCoste = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(452, 353);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(167, 94);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(63, 24);
+            this.lblTexto.TabIndex = 1;
+            this.lblTexto.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(166, 148);
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(445, 157);
+            this.txtTelegrama.TabIndex = 2;
+            this.txtTelegrama.Text = "";
+            // 
+            // chkUrgente
+            // 
+            this.chkUrgente.AutoSize = true;
+            this.chkUrgente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUrgente.Location = new System.Drawing.Point(166, 337);
+            this.chkUrgente.Name = "chkUrgente";
+            this.chkUrgente.Size = new System.Drawing.Size(114, 28);
+            this.chkUrgente.TabIndex = 3;
+            this.chkUrgente.Text = "Urgente?";
+            this.chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // lblCoste
+            // 
+            this.lblCoste.AutoSize = true;
+            this.lblCoste.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoste.Location = new System.Drawing.Point(144, 416);
+            this.lblCoste.Name = "lblCoste";
+            this.lblCoste.Size = new System.Drawing.Size(75, 24);
+            this.lblCoste.TabIndex = 4;
+            this.lblCoste.Text = "Coste: ";
+            this.lblCoste.Click += new System.EventHandler(this.lblCoste_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(239, 420);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(94, 20);
+            this.txtPrecio.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 529);
+            this.ClientSize = new System.Drawing.Size(834, 607);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblCoste);
+            this.Controls.Add(this.chkUrgente);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.btnCalcular);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.RichTextBox txtTelegrama;
+        private System.Windows.Forms.CheckBox chkUrgente;
+        private System.Windows.Forms.Label lblCoste;
+        private System.Windows.Forms.TextBox txtPrecio;
     }
 }
 
